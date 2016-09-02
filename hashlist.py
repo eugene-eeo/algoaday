ba3f4daff78c430c0d68a48d82f11fe3613a6140
Eeo Jun <141bytes@gmail.com>
2016-09-02 10:45:07 +0800

    hashlist algorithm

---
+++ hashlist.py
@@ -0,0 +1,32 @@
+from hashlib import sha256
+from itertools import chain
+
+
+class Block:
+    def __init__(self, content):
+        self.hash = sha256(content).hexdigest()
+        self.content = content
+
+
+class HashList:
+    def __init__(self, blocks):
+        self.blocks = tuple(blocks)
+        self.hash = sha256(b''.join(b.hash for b in self.blocks))
+
+    def extend(self, blocks):
+        return HashList(chain(self, blocks))
+
+    def __eq__(self, other):
+        return self.hash == other.hash
+
+
+if __name__ == '__main__':
+    h1 = HashList([
+        Block(b'abc'),
+        Block(b'def'),
+        ])
+    h2 = HashList([
+        Block(b'abc'),
+        ])
+    assert h1 != h2
+    assert h1 == h2.extend([Block(b'def')])
