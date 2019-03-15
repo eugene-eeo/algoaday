8ebd457b0ad40af190a6613ab16db16b8e00c7bf
Eeo Jun <141bytes@gmail.com>
2019-03-15 12:58:09 +0000

    fix

--- triangulate.py
+++ triangulate.py
@@ -5,19 +5,28 @@ from math import atan2, pi
 Point = namedtuple('Point', 'x,y')
 
 
+def sign(x):
+    if x == 0:
+        return 0
+    elif x < 0:
+        return -1
+    return 1
+
+
+def det(p, q, r):
+    return (q.x*r.y - q.y*r.x) - (p.x*r.y - p.y*r.x) + (p.x*q.y - p.y*q.x)
+
+
+def side(p, q, r):
+    return sign(det(p, q, r))
+
+
 def is_convex(p, q, r):
     d_in  = Point(q.x - p.x, q.y - p.y)  # p -> q
     d_out = Point(r.x - q.x, r.y - q.y)  # q -> r
-    angle = pi + atan2( d_in.x*d_out.y - d_out.x*d_in.y, d_in.x*d_out.x + d_in.y*d_out.y )
-    return angle < pi
-
-
-def triplet(points, i):
-    n = len(points)
-    x1 = points[i]
-    x2 = points[(i + 1) % n]
-    x3 = points[(i + 2) % n]
-    return x1, x2, x3
+    # angle = pi + atan2(...) < pi
+    #           => atan2(...) < 0
+    return atan2(d_in.x*d_out.y - d_out.x*d_in.y, d_in.x*d_out.x + d_in.y*d_out.y) < 0
 
 
 def triangulate(points):
@@ -28,13 +37,23 @@ def triangulate(points):
     i = 0
     while n > 3:
         i %= n
-        p, q, r = triplet(points, i)
+        j = (i + 1) % n
+        k = (i + 2) % n
+        p = points[i]
+        q = points[j]
+        r = points[k]
         if is_convex(p, q, r):
-            T.append([p, q, r])
-            # remove q
-            del points[(i + 1) % n]
-            n -= 1
-            continue
+            x = side(p, r, q)
+            # check if this is a convex subsection, i.e. triangle
+            # pqr does not intersect with any other points
+            ok = all(side(p, r, p2) != x for idx, p2 in enumerate(points)
+                     if idx not in (i, j, k))
+            if ok:
+                T.append([p, q, r])
+                # remove q
+                del points[j]
+                n -= 1
+                continue
         i += 1
     T.append(points)
     return T
